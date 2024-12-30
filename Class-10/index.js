6bb91ad617826c270bbe1c36a013a0f65b52530b
unknown <vyomraturi2004@gmail.com>
2024-12-30 20:56:52 +0530

    class 10

---
+++ Class-10/index.js
@@ -0,0 +1,6 @@
+// Task 1: Select all the required elements from the DOM and store them in variables.
+// Task 2: Create a function fetchData that will fetch the data from the weather API.
+// Task 3: Create a function search that will take the input value from the form and call the fetchData function.
+// Task 4: Add an event listener to the form that will call the search function when the form is submitted.
+// Task 5: Create a function updateDOM that will update the DOM with the fetched data.
+// Task 6: Call the fetchData function with a default city name.
